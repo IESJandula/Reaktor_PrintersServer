56a2d93ef6e5ae9f10aa674ccf7c2c25bf98966c
pakkk <paco.benitez.chico@gmail.com>
2024-05-03 17:59:04 +0200

    Adaptación al VPS

--- src/main/java/es/iesjandula/remote_printer_server/RemotePrinterServerApplication.java
+++ src/main/java/es/iesjandula/remote_printer_server/RemotePrinterServerApplication.java
@@ -2,8 +2,10 @@ package es.iesjandula.remote_printer_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "es.iesjandula.remote_printer_server")
+@ComponentScan(basePackages = {"es.iesjandula"})
 public class RemotePrinterServerApplication {
 
 	public static void main(String[] args) {
