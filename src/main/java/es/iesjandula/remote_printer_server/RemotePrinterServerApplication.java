316e9411e641020dcce3e40caa41c2e895cfe91b
pakkk <paco.benitez.chico@gmail.com>
2024-09-07 17:02:48 +0200

    Arranque automático, quitar anotación para que funcione

--- src/main/java/es/iesjandula/remote_printer_server/RemotePrinterServerApplication.java
+++ src/main/java/es/iesjandula/remote_printer_server/RemotePrinterServerApplication.java
@@ -3,8 +3,10 @@ package es.iesjandula.remote_printer_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = {"es.iesjandula"})
 public class RemotePrinterServerApplication
 {
