a72f5dea8a08b91ed80ee6dbd53728710274ef6a
Crescencio Corpas <ccorpasmena@gmail.com>
2026-05-12 19:41:44 +0200

    paginacion completa en el backend

--- src/main/java/es/iesjandula/reaktor/printers_server/repository/IPrintActionRepository.java
+++ src/main/java/es/iesjandula/reaktor/printers_server/repository/IPrintActionRepository.java
@@ -3,6 +3,8 @@ package es.iesjandula.reaktor.printers_server.repository;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -34,4 +36,30 @@ public interface IPrintActionRepository extends JpaRepository<PrintAction, Long>
 												  @Param("status") String status,
 												  @Param("startDate") Date startDate,
 												  @Param("endDate") Date endDate) ;
+
+	@Query(value = "SELECT new es.iesjandula.reaktor.printers_server.dto.ResponseDtoPrintAction(p.id, p.user, p.printer, p.status, p.fileName, "   +
+			 																		   "p.copies, p.color, p.orientation, p.sides, " +
+			 																		   "p.date, p.errorMessage, p.fileSizeInKB, " 	 + 
+			 																		   "p.numeroPaginasPdf, p.hojasTotales, p.selectedPages) "   +
+		   "FROM PrintAction p " +
+		   "WHERE " +
+				"(:user IS NULL OR p.user = :user) AND " 			+
+				"(:printer IS NULL OR p.printer = :printer) AND "   +
+				"(:status IS NULL OR p.status = :status) AND " 	    +
+				"(:startDate IS NULL OR p.date >= :startDate) AND " +
+				"(:endDate IS NULL OR p.date <= :endDate) " 		+
+		   "ORDER BY p.date DESC",
+		   countQuery = "SELECT COUNT(p) FROM PrintAction p " +
+		   				"WHERE " +
+		   					"(:user IS NULL OR p.user = :user) AND " 			+
+		   					"(:printer IS NULL OR p.printer = :printer) AND "   +
+		   					"(:status IS NULL OR p.status = :status) AND " 	    +
+		   					"(:startDate IS NULL OR p.date >= :startDate) AND " +
+		   					"(:endDate IS NULL OR p.date <= :endDate)")
+	Page<ResponseDtoPrintAction> findPrintActionsPaginated(@Param("user") String user,
+														   @Param("printer") String printer,
+														   @Param("status") String status,
+														   @Param("startDate") Date startDate,
+														   @Param("endDate") Date endDate,
+														   Pageable pageable) ;
 }
